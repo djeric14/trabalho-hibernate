2e831059196e35a1c6d1fad970ee32dfec3b0445
Alan Bacelar <me@alanbacelar.com>
2015-05-22 21:55:51 -0300

    Novo template

--- api/app/controllers/Application.java
+++ api/app/controllers/Application.java
@@ -18,67 +18,67 @@ public class Application extends Controller {
 
 
     public static Result index() {
-        PessoaDao gd = new PessoaDao();
-        gd.begin();
+        // PessoaDao gd = new PessoaDao();
+        // gd.begin();
         
-        Endereco endereco = new Endereco();
-        endereco.setLatitude(1);
-        endereco.setLongitude(1);
-        endereco.setNumero(100);
-        endereco.setLogradouro("Rua");
+        // Endereco endereco = new Endereco();
+        // endereco.setLatitude(1);
+        // endereco.setLongitude(1);
+        // endereco.setNumero(100);
+        // endereco.setLogradouro("Rua");
 
-        gd.salvar(endereco);
+        // gd.salvar(endereco);
 
-        Cliente cliente = new Cliente();
-        cliente.setCpf("34483765");
-        cliente.setDataNascimento(new Date());
-        cliente.setNome("Nome do Cliente");
-        cliente.setEndereco(endereco);
+        // Cliente cliente = new Cliente();
+        // cliente.setCpf("34483765");
+        // cliente.setDataNascimento(new Date());
+        // cliente.setNome("Nome do Cliente");
+        // cliente.setEndereco(endereco);
 
-        gd.salvar(cliente);
+        // gd.salvar(cliente);
 
-        Vendedor vendedor = new Vendedor();
-        vendedor.setNome("Nome do Vendedor");
-        vendedor.setCpf("3458495");
-        vendedor.setDataNascimento(new Date());
-        vendedor.setEndereco(endereco);
+        // Vendedor vendedor = new Vendedor();
+        // vendedor.setNome("Nome do Vendedor");
+        // vendedor.setCpf("3458495");
+        // vendedor.setDataNascimento(new Date());
+        // vendedor.setEndereco(endereco);
 
-        gd.salvar(vendedor);
+        // gd.salvar(vendedor);
 
-        Agenda agenda = new Agenda();
+        // Agenda agenda = new Agenda();
 
-        agenda.setCliente(cliente);
-        agenda.setData(new Date());
-        agenda.setVendedor(vendedor);
+        // agenda.setCliente(cliente);
+        // agenda.setData(new Date());
+        // agenda.setVendedor(vendedor);
 
-        gd.salvar(agenda);
+        // gd.salvar(agenda);
 
-        Visita visita = new Visita();
-        visita.setAgenda(agenda);
-        visita.setHoraFim(new Date());
-        visita.setHoraInicio(new Date());
-        visita.setLatitude(1.1);
-        visita.setLongitude(1.2);
+        // Visita visita = new Visita();
+        // visita.setAgenda(agenda);
+        // visita.setHoraFim(new Date());
+        // visita.setHoraInicio(new Date());
+        // visita.setLatitude(1.1);
+        // visita.setLongitude(1.2);
 
-        gd.salvar(visita);
+        // gd.salvar(visita);
 
-        Fornecedor fornecedor = new Fornecedor();
-        fornecedor.setCnpj("39823923");
-        fornecedor.setNome("Fornecedor 1");
-        fornecedor.setNomeFantasia("Nome Fantasia do Fornecedor");
-        fornecedor.setEndereco(endereco);
+        // Fornecedor fornecedor = new Fornecedor();
+        // fornecedor.setCnpj("39823923");
+        // fornecedor.setNome("Fornecedor 1");
+        // fornecedor.setNomeFantasia("Nome Fantasia do Fornecedor");
+        // fornecedor.setEndereco(endereco);
 
-        gd.salvar(fornecedor);
+        // gd.salvar(fornecedor);
 
-        Produto produto = new Produto();
-        produto.setEstoque(10);
-        produto.setFornecedor(fornecedor);
-        produto.setNome("Produto 1");
-        produto.setPreco(10.1);
+        // Produto produto = new Produto();
+        // produto.setEstoque(10);
+        // produto.setFornecedor(fornecedor);
+        // produto.setNome("Produto 1");
+        // produto.setPreco(10.1);
 
-        gd.salvar(produto);
+        // gd.salvar(produto);
 
-        gd.commit();
+        // gd.commit();
 
         return ok(index.render("My first App with Play!"));
     }
